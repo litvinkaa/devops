57d36c7b3b60562e67031a66d15fbf22db491056
unknown <litvinka42@gmail.com>
2023-01-17 17:41:11 +0200

    fixed Jenkinsfile

--- coursework/terraform/main.tf
+++ coursework/terraform/main.tf
@@ -35,8 +35,8 @@ resource "aws_security_group" "sg" {
     cidr_blocks = [
       "0.0.0.0/0"
     ]
-    from_port = 5000
-    to_port = 5000
+    from_port = 3000
+    to_port = 3000
     protocol = "tcp"
   }// Terraform removes the default rule
   egress {
@@ -50,7 +50,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "terraform"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9KlzAo+MaHoWXQm2lI8ngU3jmZyiLVX5jqorlGHa5fah5AT3Td+euxFFYPeEB49rgf4FiI2OQUPLMg4lAwYrrbtaqsXzU0Rj1fco0dll14cAJqoh/Xzcob1hmdzNcfxeMgj/tZefwEWMZ5Lh+2+pS67WnrrGbydFAq/VX78LJsCzDweYmxo3JP62pKBdjopZsy3raOrLDtB0RPBxHExvRdmlig1cRBbgh5onTutqGOiHinjah7XmNZk4GztPehb6U4H/jA4Sui9f3bVGBKnrBDDWOlkPbpl1NTHCY76IO51bnlpINTrkjRvhi5TM7FzltZArfoZOWQnjJwxjwia21rTchrQr20INkTydBNPQ6ZnMnJKiGpp5pStwO+zRPBnMe4p/ByJVAnrDgMs1pnxJ0vAYIeaNUVhMhcaHOOJf6Zk0FfHiDRP6eNH6izGir7YM/nSRqjSu/KSYGvnCJp7YBPJ54F2EA9RVWN9BBny49hocqTBFmzWC9HZHxn2iRWvU= misha@DESKTOP-F4GIVBJ"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0QLNwo7+pqg2qVL9bFgGsTKnVnN1hdmkPGJMJdFJj2O0cVZTebYwjdBW4o0DZjBsWFjyDwZIZZMRiAmbfbxO/fcRlKJ6A3MScS+BTVyFEsCOLSyvCvmv/OAGIaKTi0zUnUQwjgoOLObycgvazV98IN74qW5b5kVAVnukyjOjTSakXr3+wcQfxTZhm9jJ0+r1/1U2DdVvjjPIEQ4peEHWeOg0Ggw3jnjhecSmKy6kOUweOeuBOo2GKn6X52se8ngZ7VvyGQQNSAeuwS0aMvL3XG+cOEEn6xyW2fVAE+vN2b3gx2DuKRimALdXomGryZBNfWFPS7xSh1CXwEwMTGHyaaAcUG1ryEbyRZ5PQ5LWCmF1pa72uRZuVE+/6dIzETC3dyXn7eRWQFRfr3q00+2KNi06DiGEpGfq6pq4Ppl3c7qGDwKqicGavjdRTzgWiuDIEOIHU2T2kusFV7F/Zxa8B3TFQvUNG0xA7welm8hOmmxiXUZjzfpVQpyD85LAhR0s= misha@DESKTOP-F4GIVBJ"
 }
 
 resource "aws_instance" "app_server" {
