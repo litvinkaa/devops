5d925c78f92039a1407d846f1cd6fad9e9fed7d2
unknown <litvinka42@gmail.com>
2023-01-17 15:07:51 +0200

    Jenkins pipeline + app

--- coursework/app/app.js
+++ coursework/app/app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("")
+  res.send("Hello there!")
 })
 
 app.listen(port, () => {
-  
+  console.log(`Listening on port ${port}`)
 })
